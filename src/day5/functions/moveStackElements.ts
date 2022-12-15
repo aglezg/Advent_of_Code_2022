88a9fe5b34eff22b52296b7af6743de01df939bb
Adrián González Galván <alu0101321219@ull.edu.es>
2022-12-15 12:42:15 +0000

    Day 5 completed

--- src/day5/functions/moveStackElements.ts
+++ src/day5/functions/moveStackElements.ts
@@ -4,14 +4,29 @@ export type crate = "A" | "B" | "C" | "D" | "E" | "F" | "G" | "H" | "I" | "J" | 
 | "L" | "M" | "N" | "O" | "P" | "Q" | "R" | "S" | "T" | "U" | "V" | "W" | "X" 
 | "Y" | "Z";
 
-export function moveStackElements(stack1: Stack<crate>, stack2: Stack<crate>, n: number): void {
-  for (let i: number = 0; i < n; i++) {
-    if (stack1.length != 0) {
-      stack2.push(stack1.top);
-      stack1.pop();
-    } else {
-      console.log('error: empty stack...');
-      break;
+export function moveStackElements(stack1: Stack<crate>, stack2: Stack<crate>, n: number, newVersion: boolean = false): void {
+  if (newVersion == false) {
+    for (let i: number = 0; i < n; i++) {
+      if (stack1.length != 0) {
+        stack2.push(stack1.top);
+        stack1.pop();
+      } else {
+        console.log('error: empty stack...');
+        break;
+      }
     }
+  } else {
+    let stackVector: crate[] = [];
+    for (let i: number = 0; i < n; i++) {
+      if (stack1.length != 0) {
+        stackVector.unshift(stack1.pop());
+      } else {
+        console.log('error: empty stack...');
+        break;
+      }
+    }
+    stackVector.forEach((element: crate) => {
+      stack2.push(element);
+    })
   }
 }
