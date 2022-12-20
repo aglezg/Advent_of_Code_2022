e03bd0d686a760533b51c041120ebb07520fd1ac
Adrián González Galván <glezgalvan.adrian@gmail.com>
2022-12-20 09:58:54 +0000

    Day 6 completed

---
+++ src/day6/day6_2.ts
@@ -0,0 +1,33 @@
+import { existsSync, readFileSync } from "fs";
+import { argv } from "process";
+
+// Lectura
+let input: string = argv[2];
+let data: string = "";
+
+if (!existsSync(input)) {
+  console.error("err: file does not exist")
+} else {
+  data = readFileSync(input).toString();
+}
+
+// Number of characters to check
+let n: number = 14;
+
+// Set of characters
+let charactersSet: Set<string> = new Set<string>();
+
+// Recorremos los datos leidos
+for(let i: number = 0; i < data.length; i++) {
+    let count: number = 0;
+    while (count < n) {
+        charactersSet.add(data[i + count]);
+        count++;
+    }
+    if (charactersSet.size == n) {
+        console.log(i + n);
+        break;
+    } else {
+        charactersSet = new Set<string>();
+    }
+}
