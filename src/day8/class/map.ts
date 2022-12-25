12524c43ca35fe5cb955a3938e9f97b9eda983fd
Adrián González Galván <glezgalvan.adrian@gmail.com>
2022-12-25 14:24:31 +0000

    Day 8 finished (error answer)

--- src/day8/class/map.ts
+++ src/day8/class/map.ts
@@ -5,8 +5,30 @@ export type Direction = "N" | "S" | "E" | "W";
  * @param matrix Map of tree with them heigths
  */
 export class Map {
+  /**
+   * Constructor
+   * @param matrix Matrix with tree heights 
+   */
   constructor(public matrix: number[][]) {}
 
+  /**
+   * Check if a Tree is lower than other
+   * @param i 'i' position of first tree
+   * @param j 'j' position of first tree
+   * @param i2 'i' position of second tree
+   * @param j2 'j' position of second tree
+   * @returns True if first tree is lower than second tree
+   */
+  public isLower(i: number, j: number, i2: number, j2: number) {
+    return this.matrix[i][j] < this.matrix[i2][j2];
+  }
+
+  /**
+   * Check if a Tree is Visible
+   * @param i 'i' position
+   * @param j 'j' position
+   * @returns True if its visible
+   */
   public isVisible(i: number, j: number) {
     if (this.isExtreme(i, j)) {
       return true;
@@ -16,59 +38,68 @@ export class Map {
     }
   }
 
+  /**
+   * Calculate the tree scored based on the proximity tree heights
+   * @param i 'i' position of tree
+   * @param j 'j' position of tree
+   * @returns Return the score
+   */
   public getScore(i: number, j: number): number {
     return this.getScoreFrom(i, j, "N") * this.getScoreFrom(i, j, "S") *
       this.getScoreFrom(i, j, "E") * this.getScoreFrom(i, j, "W");
   }
 
+  /**
+   * Get a tree score from an especific direction
+   * @param i 'i' position of tree
+   * @param j 'j' position of tree
+   * @param dir Direction to use
+   * @returns Score from that direction
+   */
   private getScoreFrom(i: number, j: number, dir: Direction): number {
     let score: number = 0;
     switch(dir) {
       case "N":
-        for (let a: number = i - 1; a >= 0; a--)
-          if (this.matrix[a][j] < this.matrix[i][j]) {
+        for (let k: number = i - 1; k >= 0; k--)
+          if (this.isLower(k, j, i, j)) {
             score++;
-          } else if (this.matrix[a][j] == this.matrix[i][j]) {
-            score ++;
+          } else if (this.matrix[k][j] == this.matrix[i][j]) {
+            score++;
             break;
           } else {
-            score++;
             break;
           }
         break;
       case "S":
         for (let a: number = i + 1; a < this.matrix.length; a++)
-          if (this.matrix[a][j] < this.matrix[i][j]) {
+          if (this.isLower(a, j, i, j)) {
             score++;
           } else if (this.matrix[a][j] == this.matrix[i][j]) {
             score ++;
             break;
           } else {
-            score++;
             break;
           }
         break;
       case "E":
         for (let a: number = j + 1; a < this.matrix[i].length; a++)
-          if (this.matrix[i][a] < this.matrix[i][j]) {
+          if (this.isLower(i, a, i, j)) {
             score++;
           } else if (this.matrix[i][a] == this.matrix[i][j]) {
             score ++;
             break;
           } else {
-            score++;
             break;
           }
         break;
       case "W":
         for (let a: number = j - 1; a >= 0; a--)
-          if (this.matrix[i][a] < this.matrix[i][j]) {
+          if (this.isLower(i, a, i, j)) {
             score++;
           } else if (this.matrix[i][a] == this.matrix[i][j]) {
             score ++;
             break;
           } else {
-            score++;
             break;
           }
         break;
@@ -76,6 +107,12 @@ export class Map {
     return score;
   }
 
+  /**
+   * Determine if a tree is in a extreme position
+   * @param i 'i' Position
+   * @param j 'j' Position
+   * @returns True if the tree is located in a extreme
+   */
   private isExtreme(i: number, j: number): boolean {
     if (i == 0 || j == 0 || i == this.matrix.length - 1 || j == this.matrix[i].length - 1) {
       return true;
@@ -84,38 +121,37 @@ export class Map {
     }
   }
 
+  /**
+   * Check if a Tree is visible from one direction
+   * @param i 'i' position
+   * @param j 'j' position
+   * @param dir Direction to check
+   * @returns True if is visible from that direction
+   */
   private isVisibleFrom(i: number, j: number, dir: Direction): boolean {
     if (this.isExtreme(i, j)) {
       return true;
     } else {
       switch(dir) {
         case "N":
-          for (let a: number = i - 1; a >= 0; a--) {
-            if (this.matrix[a][j] >= this.matrix[i][j]) {
+          for (let k: number = i - 1; k >= 0; k--)
+            if (!this.isLower(k, j, i, j))
               return false;
-            }
-          }
           return true;
         case "S":
-          for (let a: number = i + 1; a < this.matrix.length; a++) {
-            if (this.matrix[a][j] >= this.matrix[i][j]) {
+          for (let k: number = i + 1; k < this.matrix.length; k++)
+            if (!this.isLower(k, j, i, j))
               return false;
-            }
-          }
           return true;
         case "E":
-          for (let a: number = j + 1; a < this.matrix[i].length; a++) {
-            if (this.matrix[i][a] >= this.matrix[i][j]) {
+          for (let k: number = j + 1; k < this.matrix[i].length; k++)
+            if (!this.isLower(i, k, i, j))
               return false;
-            }
-          }
           return true;
         case "W":
-          for (let a: number = j - 1; a >= 0; a--) {
-            if (this.matrix[i][a] >= this.matrix[i][j]) {
+          for (let k: number = j - 1; k >= 0; k--)
+            if (!this.isLower(i, k, i, j))
               return false;
-            }
-          }
           return true;
       }
     }
--- src/day8/day8_2.ts
+++ src/day8/day8_2.ts
@@ -29,16 +29,19 @@ lines.forEach((line: string) => {
 });
 
 // CALCULOUS
+
 let maxScore: number = -1;
 let myMap: Map = new Map(matrix);
+
 for (let i: number = 0; i < myMap.matrix.length; i++) {
   for (let j: number = 0; j < myMap.matrix[i].length; j++) {
     let score: number =  myMap.getScore(i, j);
-    console.log("SCORE (, ", i, ", ", j, ")>> ", score);
-    if ( score > maxScore) {
+    if (score > maxScore) {
       maxScore = score;
+      console.log("New max score!! >> ", maxScore);
+      console.log("From (", i, ", ", j, ")");
     }
   }
 }
 
-console.log(myMap.getScore(97, 96));
+console.log("Answer: ", maxScore);
